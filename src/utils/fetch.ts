d7e07f1f34df6f3a3ad43a9b6beb50b9f80ff1b4
Hryshko Denys <gryshko.denys@gmail.com>
2023-01-22 21:17:11 +0200

    feat: add netlify.toml file

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,7 +1,19 @@
 import { SongInfoInt, TrackItemInt } from "../types/TrackTypes";
 
+// let headers = new Headers();
+// //
+// headers.append("Content-Type", "application/json");
+// headers.append("Accept", "application/json");
+// headers.append("Origin", "http://localhost:5173");
+// headers.append("Access-Control-Allow-Origin", "*");
+
 export const fetchSongs = async () => {
-  const response = await fetch(import.meta.env.VITE_RADIO_SONGS_API);
+  const response = await fetch(import.meta.env.VITE_RADIO_SONGS_API, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
   const responseData = await response.json();
 
   return responseData?.nowplaying;
@@ -13,7 +25,8 @@ export const findSongAlbum = async (songInfo: TrackItemInt) => {
     .replaceAll(/[^a-zA-Z0-9]/g, "");
   const searchParams = titleParams;
   const songResponse = await fetch(
-    `${import.meta.env.VITE_APPLE_SONGS_API}${searchParams}`
+    `${import.meta.env.VITE_APPLE_SONGS_API}${searchParams}`,
+    { method: "GET", mode: "no-cors" }
   );
   const songData = await songResponse.json();
 
